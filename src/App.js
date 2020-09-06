c16533d418801605b52fe511ed363eaa309cf848
nicovernot <nicovernot@gmail.com>
2020-09-06 18:43:43 +0200

    upd loggin

--- src/App.js
+++ src/App.js
@@ -111,14 +111,16 @@ window.location.href = "http://www.w3schools.com";
       console.log(process.env.REACT_APP_URL_HOST)
       axios.post(`http://`+process.env.REACT_APP_URL_HOST+`/auth/local`, { "identifier":email.email,"password":pwd.pwd })
       .then(res => {
-     
+     console.log(res)
         if(res.data.jwt){
           console.log(res.data.jwt);
+
           localStorage.setItem("username", email.email); 
           localStorage.setItem("token", res.data.jwt); 
           this.setState({modalvisible: false})
           this.setState({user : {email: email.email,  logged :true }})
           this.setState({user:{logginerror:false}})
+          document.location.reload(true);
         } 
       }).catch((error) => {
         console.log(error)
