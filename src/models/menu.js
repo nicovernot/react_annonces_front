e2a9a707a5c881c43f1317e04cda5dd799f2c09d
nicovernot <nicovernot@gmail.com>
2020-09-08 08:17:07 +0200

    bootstrap  react npm

--- src/models/menu.js
+++ src/models/menu.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
 import {
    
     Link
@@ -29,25 +31,19 @@ const Menu = (props) => {
     ) 
   
     return (
-      <div>
-
-      <nav className="navbar navbar-expand-md bg-dark navbar-dark">
-  <a className="navbar-brand" href="/">Redma</a>
-
-
-  
-  <div className="collapse navbar-collapse" id="collapsibleNavbar">
-    <ul className="navbar-nav">
-      
+<Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" >
+  <Navbar.Brand href="#home">Redma</Navbar.Brand>
+  <Navbar.Collapse  id="responsive-navbar-nav">
+    <Nav className="mr-auto">
      {menulist}
-    </ul>
-  </div>
-        {props.children}
-  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-    <span className="navbar-toggler-icon"></span>
-  </button>
-</nav> 
-      </div>
+
+    </Nav>
+    
+
+  </Navbar.Collapse>
+  {props.children}
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+</Navbar>
        
          
 
